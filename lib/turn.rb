871b5205079720d1f47ca364454049a76ed22d03
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-12 20:08:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def position_taken?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:" #ask for input         
+  puts "Please enter 1-9:" #ask for input
 input = input_to_index(user_input) #get input
   input_to_index(user_input)
 
